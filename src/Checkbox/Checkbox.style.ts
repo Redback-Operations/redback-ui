25a8349a8b637e87a84006be4ae5500e81aa6fbe
TDonks <112368401+TDonks@users.noreply.github.com>
2024-05-21 23:16:50 +1000

    Add files via upload

---
+++ src/Checkbox/Checkbox.style.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import { ThemeColor, ThemeElementAppearance, ThemeElementSize } from '../../types.ts';
+import { readableColor, shade } from 'polished';
+
+type StyledButtonProps = {
+  $color: ThemeColor;
+  $appearance: ThemeElementAppearance;
+  $size: ThemeElementSize;
+};
+
+export const StyledCheckbox = styled.button<StyledButtonProps>`
+  display: inline-block;
+  background: ${props => props.theme.colors[props.$color]};
+  color: ${props => readableColor(props.theme.colors[props.$color])};
+  appearance: none;
+  border: 0;
+  border-radius: 0.25rem; 
+  cursor: pointer;
+  transition: all 0.3s ease;
+  
+  // Handle appearance
+  ${props => {
+    if (props.$appearance === 'outline') {
+      return `
+        background: transparent;
+        color: ${props.theme.colors[props.$color]};
+        border: 1px solid ${props.theme.colors[props.$color]};
+      `;
+    }
+  }};
+
+  // Handle sizes 
+  ${props => {
+    if (props.$size === 'sm') {
+      return `
+        width: 1rem;
+        height: 1rem;
+      `;
+    }
+    if (props.$size === 'lg') {
+      return `
+        width: 2rem;
+        height: 2rem;
+      `;
+    }
+    return `
+      width: 1.5rem;
+      height: 1.5rem;
+    `;
+  }};
+
+  &:hover, &:focus, &:active {
+    text-decoration-color: currentColor;
+    color: ${props => readableColor(props.theme.colors[props.$color])};
+    
+    ${props => props.$appearance === 'outline' ? `
+      background: ${props.theme.colors[props.$color]};
+    ` : `
+      background: ${shade(0.15, props.theme.colors[props.$color])};
+    `}
+  }
+`;
