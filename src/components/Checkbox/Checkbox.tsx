eea8de45f46f23441020008eb828c943d94f4463
TDonks <112368401+TDonks@users.noreply.github.com>
2024-05-21 23:03:39 +1000

    Add files via upload
    added checkbox and switch UI

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import { StyledCheckbox } from './Checkbox.style';
+import { ThemeColor, ThemeElementAppearance, ThemeElementSize } from '../../types.ts';
+
+type CheckboxProps = {
+	color?: ThemeColor;
+	onClick: () => void;
+	appearance?: ThemeElementAppearance;
+	size?: ThemeElementSize;
+}
+
+const Checkbox: FC<CheckboxProps> = ({
+	color = 'primary',
+	onClick,
+	appearance = 'solid',
+	size = 'md'
+}) => {
+	return (
+		<StyledCheckbox data-testid="Button" onClick={onClick} $color={color} $appearance={appearance} $size={size}>
+			{}
+		</StyledCheckbox>
+	);
+};
+
+export default Checkbox;
