bddee4bf7bc58abf7883e39fab22489da3dda020
TDonks <112368401+TDonks@users.noreply.github.com>
2024-05-21 22:59:41 +1000

    Add files via upload
    UI for checkboxes and switches

---
+++ Checkbox/Checkbox.stories.ts
@@ -0,0 +1,76 @@
+import Checkbox from './Checkbox';
+import { fn } from '@storybook/test';
+import type { Meta, StoryObj } from '@storybook/react';
+import { themeColorSubset } from '../../types.ts';
+
+const meta = {
+	title: 'Components/Checkbox',
+	component: Checkbox,
+	argTypes: {
+		color: { control: 'select', options: Object.keys(themeColorSubset) },
+	},
+	args: {
+		onClick: fn() // spy on the onClick arg, so it will appear in the actions panel once invoked
+	},
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const defaultProps = {
+	onClick: fn()
+};
+
+const disableControls = {
+	parameters: {
+		controls: {
+			disable: true
+		},
+		actions: {
+			disable: true
+		},
+	}
+};
+
+export const Demo: Story = {
+	args: {
+		...defaultProps,
+	},
+	tags: ['excludeFromSidebar']
+};
+
+export const Default: Story = {
+	args: {
+		...defaultProps,
+		color: 'primary',
+	},
+	...disableControls
+};
+
+export const Secondary: Story = {
+	args: {
+		...defaultProps,
+		color: 'secondary',
+		onClick: fn()
+	},
+	...disableControls
+};
+
+export const PrimaryOutline: Story = {
+	args: {
+		...defaultProps,
+		color: 'primary',
+		appearance: 'outline'
+	},
+	...disableControls
+};
+
+export const SecondaryOutline: Story = {
+	args: {
+		...defaultProps,
+		color: 'secondary',
+		appearance: 'outline'
+	},
+	...disableControls
+};
+
