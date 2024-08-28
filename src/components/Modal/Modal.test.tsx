546ac8da9e9e3f6a0b2eb895b7072ebf7046353f
Narinder <85366812+Narinder788@users.noreply.github.com>
2024-08-29 07:22:40 +1000

    FIX: issues with import in index and modal file

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -1,6 +1,6 @@
 import { screen } from '@testing-library/react';
-import { renderWithDeps } from '../../../jest.utils.tsx';
-import Modal from './Modal';
+import { renderWithDeps } from '../../../jest.utils';
+import { Modal } from './Modal';
 
 describe('<Modal />', () => {
 	it('renders', () => {
