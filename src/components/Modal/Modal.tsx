10bd2d71d0312a6f7ba87a38b37915e4e3ca2597
Narinder <85366812+Narinder788@users.noreply.github.com>
2024-09-02 10:17:34 +1000

    UPDATE: complete the closing functionality for modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
-import { StyledModal } from './Modal.style';
+import { StyledModal, StyledBackdrop } from './Modal.style';
 
 export const Modal: React.FC = () => {
 	const [open, setOpen] = useState(false);
 
 	const handleOpen = () => setOpen(true);
-	// const handleClose = () => setOpen(false);
+	const handleClose = () => setOpen(false);
 
 	return (
 		<div data-testid="Modal">
 			<button onClick={handleOpen}>Open modal</button>
 			{open && (
 				<>
-					{/* <StyledBackdrop onClick={handleClose} /> */}
-					<StyledModal>
+					<StyledModal onClick={handleClose}>
 						<h2 id="modal-title">Text in a modal</h2>
 						<p id="modal-description">
 							Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
 						</p>
+						<button onClick={handleClose}>Close</button>
 					</StyledModal>
 				</>
 			)}
