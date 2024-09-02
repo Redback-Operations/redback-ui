b8c470a56a38e6435901d0930a0f564df67ca6b3
Narinder <85366812+Narinder788@users.noreply.github.com>
2024-09-02 10:36:54 +1000

    ADD: more test for modal component open and close

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -1,5 +1,5 @@
-import { screen } from '@testing-library/react';
 import { renderWithDeps } from '../../../jest.utils';
+import { fireEvent, screen } from '@testing-library/react';
 import { Modal } from './Modal';
 
 describe('<Modal />', () => {
@@ -9,5 +9,34 @@ describe('<Modal />', () => {
 		const modal = screen.getByTestId('Modal');
 
 		expect(modal).toBeInTheDocument();
+	});
+
+	it('opens the modal when clicking the Open button', () => {
+		renderWithDeps(<Modal />);
+
+		const openButton = screen.getByText('Open modal');
+		fireEvent.click(openButton);
+
+		// Check if the modal content is visible
+		const modalTitle = screen.getByText('Text in a modal');
+		expect(modalTitle).toBeVisible();
+	});
+
+	it('closes the modal when clicking the Close button', () => {
+		renderWithDeps(<Modal />);
+
+		const openButton = screen.getByText('Open modal');
+		fireEvent.click(openButton);
+
+		// Check if the modal is opened
+		const modalTitle = screen.getByText('Text in a modal');
+		expect(modalTitle).toBeVisible();
+
+		// Click the close button
+		const closeButton = screen.getByText('Close');
+		fireEvent.click(closeButton);
+
+		// Check if the modal content is no longer visible
+		expect(modalTitle).not.toBeVisible();
 	});
 });
