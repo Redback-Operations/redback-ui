25f58b799b565b85ab18cb3491853eb40d7e8431
Ashwathi23 <139518396+Ashwathi23@users.noreply.github.com>
2024-09-10 16:28:21 +1000

    Update Accordion.test.tsx

--- src/components/Accordion/Accordion.test.tsx
+++ src/components/Accordion/Accordion.test.tsx
@@ -2,19 +2,19 @@ import { render, fireEvent } from '@testing-library/react';
 import { Accordion } from './Accordion';
 
 describe('<Accordion />', () => {
-  it('renders the accordion component', () => {
-    const { getByText } = render(
-      <Accordion items={[{ title: 'Title', content: 'Content' }]} />
-    );
-    expect(getByText('Title')).toBeInTheDocument();
-  });
+	it('renders the accordion component', () => {
+		const { getByText } = render(
+  		<Accordion items={[{ title: 'Title', content: 'Content' }]} />
+		);
+		expect(getByText('Title')).toBeInTheDocument();
+	});
 
-  it('toggles content on click', () => {
-    const { getByText } = render(
-      <Accordion items={[{ title: 'Title', content: 'Content' }]} />
-    );
-    const titleElement = getByText('Title');
-    fireEvent.click(titleElement);
-    expect(getByText('Content')).toBeInTheDocument();
-  });
+	it('toggles content on click', () => {
+		const { getByText } = render(
+			<Accordion items={[{ title: 'Title', content: 'Content' }]} />
+		);
+		const titleElement = getByText('Title');
+		fireEvent.click(titleElement);
+		expect(getByText('Content')).toBeInTheDocument();
+	});
 });
