dcd2448d9f2e3440e9a990c2935bcc94deda0dc5
doubleedesign <doubleedesign@users.noreply.github.com>
2024-09-14 00:55:03 +0000

    deploy: 91e78ad214eb2e107521f6f89913d4634582429c

--- assets/DocsRenderer-PKQXORMH-BIhibD1L.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./index-EeWIkZEf.js","./index-mfbfsM12.js","./_commonjsHelpers-BosuxZz1.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as p}from"./iframe-BS3-KVV6.js";import{R as e,r as c}from"./index-mfbfsM12.js";import{r as l,u}from"./react-18-B0zmDqTm.js";import{ae as h,af as E,ag as d,ah as x}from"./index-DMudKTeD.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-CnQXyiSh.js";import"./polished.esm-B9vzHjoG.js";import"./hasIn-CZcrsiw3.js";import"./_getPrototype-DHgE_lbf.js";import"./index-uexYa9xf.js";import"./index-DrFu-skq.js";var _={code:h,a:E,...d},f=class extends c.Component{constructor(){super(...arguments),this.state={hasError:!1}}static getDerivedStateFromError(){return{hasError:!0}}componentDidCatch(t){let{showException:r}=this.props;r(t)}render(){let{hasError:t}=this.state,{children:r}=this.props;return t?null:e.createElement(e.Fragment,null,r)}},T=class{constructor(){this.render=async(t,r,o)=>{let n={..._,...r==null?void 0:r.components},s=x;return new Promise((m,a)=>{p(()=>import("./index-EeWIkZEf.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(({MDXProvider:i})=>l(e.createElement(f,{showException:a,key:Math.random()},e.createElement(i,{components:n},e.createElement(s,{context:t,docsParameter:r}))),o)).then(()=>m())})},this.unmount=t=>{u(t)}}};export{T as DocsRenderer,_ as defaultComponents};
